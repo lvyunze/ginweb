6aa3fbf684bdcd4d656eab024ea801d95e8594f5
milagin <xiaoze123456789@gmail.com>
2023-10-29 01:45:07 +0800

    add viper

--- main.go
+++ main.go
@@ -5,8 +5,18 @@ import (
 	"github.com/spf13/viper"
 )
 
+type MySQL struct {
+	Host     string
+	DbName   string
+	Port     int
+	usename  string
+	Password string
+	Charset  string
+}
+
 func main() {
 	// 创建一个默认的路由引擎
+	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 	viper.ReadInConfig()

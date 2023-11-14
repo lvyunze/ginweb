c274d8e8615a51b83386b5e20df92f7460b3bb9c
milagin <xiaoze123456789@gmail.com>
2023-11-14 22:05:23 +0800

    add rbac router

--- routers/default.go
+++ routers/default.go
@@ -3,8 +3,28 @@ package routers
 import "github.com/gin-gonic/gin"
 
 func DefaultRoutersInit(r *gin.Engine) {
-	defaultRouters := r.Group("/api/v1/default")
+	defaultRouters := r.Group("/api/v1")
 	{
-		defaultRouters.GET("/")
+		user := defaultRouters.Group("/user")
+		{
+			user.POST("/getUser")
+			user.POST("updateUser")
+			user.POST("deleteUser")
+			user.POST("addUser")
+		}
+		role := defaultRouters.Group("/role")
+		{
+			role.POST("/addRole")
+			role.POST("/deleteRole")
+			role.POST("/updateRole")
+			role.POST("/getRole")
+		}
+		permission := defaultRouters.Group("/permission")
+		{
+			permission.POST("/addPermission")
+			permission.POST("/deletePermission")
+			permission.POST("/updatePermission")
+			permission.POST("/getPermission")
+		}
 	}
 }

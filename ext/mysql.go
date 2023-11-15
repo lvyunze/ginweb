9cf48a9d8628b417a6011e0560a03f7aefc3e672
milagin <xiaoze123456789@gmail.com>
2023-11-15 22:43:18 +0800

    add rbac router

---
+++ ext/mysql.go
@@ -0,0 +1,37 @@
+package ext
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var err error
+
+func init() {
+	dbHost := viper.GetString("MySQL.Host")
+	dbPort := viper.GetString("MySQL.Port")
+	dbUser := viper.GetString("MySQL.Username")
+	dbPassword := viper.GetString("MySQL.Password")
+	dbName := viper.GetString("MySQL.DbName")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Printf("无法连接到MySQL数据库：%v", err)
+		panic("无法连接到MySQL数据库")
+	}
+
+	// 设置连接池参数
+	sqlDB, err := DB.DB()
+	if err != nil {
+		fmt.Printf("获取数据库连接池失败：%v", err)
+		panic("获取数据库连接池失败")
+	}
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+}
--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"ginweb/ext"
 	"ginweb/utils/common"
 	"gorm.io/gorm"
 )
@@ -20,4 +21,36 @@ func (u *User) Serialize() common.JSON {
 		"username":     u.Username,
 		"display_name": u.DisplayName,
 	}
+}
+
+func GetUser() (userList []*User, err error) {
+	if err = ext.DB.Find(&userList).Error; err != nil {
+		return nil, err
+	}
+	return
+}
+
+func UpdateUser() (userList []*User, err error) {
+	if err = ext.DB.Find(&userList).Error; err != nil {
+		return nil, err
+	}
+	return
+}
+
+func GetAUser(id string) (user *User, err error) {
+	user = new(User)
+	if err = ext.DB.Where("id=?", id).First(user).Error; err != nil {
+		return nil, err
+	}
+	return
+}
+
+func UpdateAUser(user *User) (err error) {
+	err = ext.DB.Save(user).Error
+	return
+}
+
+func DeleteAUser(id string) (err error) {
+	err = ext.DB.Where("id=?", id).Delete(&User{}).Error
+	return
 }
